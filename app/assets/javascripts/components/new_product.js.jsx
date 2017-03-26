0bcfdfdd11f05b9baa81800fe8ef736d73f0f330
Miron Marczuk <miron.marczuk@gmail.com>
2017-03-26 01:00:42 +0100

    Basic new item form and addition handling on server side

--- app/assets/javascripts/components/new_product.js.jsx
+++ app/assets/javascripts/components/new_product.js.jsx
@@ -1,29 +1,115 @@
 var NewProduct = React.createClass({
-  propTypes: {
-    name: React.PropTypes.string,
-    company: React.PropTypes.string,
-    category: React.PropTypes.string,
-    amount: React.PropTypes.number,
-    cardinality: React.PropTypes.number,
-    unit: React.PropTypes.string,
-    dueDate: React.PropTypes.string
+  getInitialState: function(){
+    return {  isClosed: true,
+              name: '',
+              company: '',
+              category: '',
+              amount: '',
+              cardinality: '',
+              unit: '',
+              dueDate: ''};
+  },
+
+  _changeClosingState: function(e) {
+    const _isClosed = !this.state.isClosed;
+    this.setState({isClosed: _isClosed});
+  },
+
+  _onFieldChange: function(e) {
+    var input_name = e.target.name;
+    var value = e.target.value;
+    this.setState({ [input_name] : value });
   },
 
   _addProduct: function(e) {
+    e.preventDefault();
+    if(this._checkFields())
+    {
+      const data = {
+        name: this.state.name,
+        company: this.state.company,
+        category: this.state.category,
+        amount: this.state.amount,
+        cardinality: this.state.cardinality,
+        unit: this.state.unit,
+        due_date: this.state.dueDate
+      }
 
+      this._handleProductAdd(data);
+    } else
+    {
+      console.log("Odpowiednieola nie są wypełnione");
+    }
+  },
+
+  _checkFields: function() {
+    /*Tu bedzie sprawdzenie, czy odpowiednie pola sa wypelnione*/
+    return true;
+  },
+
+  _handleProductAdd: function(data) {
+    self = this;
+    $.ajax({
+      url: '/items',
+      method: 'POST',
+      data: {item: data},
+      success: function(data) {
+        //self._updateAccomodations(data);
+        console.log("Udało się dodać nowy produkt!");
+      },
+      error: function(xhr, status, error) {
+        alert('Create not successful! ', error);
+      }
+    });
   },
 
   render: function() {
+    const isClosed = this.state.isClosed;
     return (
-      <div>
-        <div>Name: {this.props.name}</div>
-        <div>Company: {this.props.company}</div>
-        <div>Category: {this.props.category}</div>
-        <div>Amount: {this.props.amount}</div>
-        <div>Cardinality: {this.props.cardinality}</div>
-        <div>Unit: {this.props.unit}</div>
-        <div>Due Date: {this.props.dueDate}</div>
-        <button>Send</button>
+      <div className="row">
+        <div className="col-md-6 col-md-offset-3 borderrr">
+      {isClosed ? (
+        <button className="btn btn-success" onClick={this._changeClosingState}>Open form </button>
+      ) : (
+          <form onSubmit={this._addProduct}>
+            <div className="form-group">
+              <label for="nameInput">Name</label>
+              <input  type="text"
+                      class="form-control"
+                      id="nameInput"
+                      value={this.props.name}
+                      onChange={this._onFieldChange}
+                      placeholder="Name"/>
+            </div>
+            <div className="form-group">
+              <label for="companyInput">Company</label>
+              <input type="text" class="form-control" id="companyInput" value={this.props.company} onChange={this._onFieldChange} placeholder="Company"/>
+            </div>
+            <div className="form-group">
+              <label for="categoryInput">Category</label>
+              <input type="text" class="form-control" id="categoryInput" value={this.props.category} onChange={this._onFieldChange} placeholder="Category"/>
+            </div>
+            <div className="form-group">
+              <label for="amountInput">Amount</label>
+              <input type="number" class="form-control" id="amountInput" value={this.props.amount} onChange={this._onFieldChange} placeholder="Amount"/>
+            </div>
+            <div className="form-group">
+              <label for="cardinalityInput">Cardinality</label>
+              <input type="number" class="form-control" id="cardinalityInput" value={this.props.cardinality} onChange={this._onFieldChange} placeholder="Cardinality"/>
+            </div>
+            <div className="form-group">
+              <label for="unitInput">Unit</label>
+              <input type="text" class="form-control" id="unitInput" value={this.props.unit} onChange={this._onFieldChange} placeholder="Unit"/>
+            </div>
+            <div className="form-group">
+              <label for="dueDateInput">Due Date</label>
+              <input type="text" class="form-control" id="dueDateInput" value={this.props.dueDate} onChange={this._onFieldChange} placeholder="Due Date"/>
+            </div>
+            <button type="submit" className="btn btn-success">Send</button>
+            <button type="button" className="btn btn-default" onClick={this._changeClosingState}>Close</button>
+          </form>
+        )}
+        </div>
       </div>
     );
   }
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,20 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render json: item.as_json
+    else
+      render nothing: true, status: bad_request
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :company, :category, :amount, :cardinality, :unit, :due_date)
+  end
+
 end
